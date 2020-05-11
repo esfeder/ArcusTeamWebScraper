55d28a09bd1ec79f108fe7186424370f3f9b6331
Eli Feder <esf10@duke.edu>
2020-05-11 19:12:48 -0400

    figured out multiple pages and getting product urls

--- WEB CRAWLER PY.py
+++ WEB CRAWLER PY.py
@@ -1,27 +1,67 @@
+# import the libraries
 import pymongo
 import requests
+import json
 from bs4 import BeautifulSoup
 
-#Establish connection with default host and port 
-client = MongoClient()
-
-#Create database
-db = client.firmware_db
-
-#Create a collection
-collection = db.firmware_collection
+host = 'localhost'
+database = 'firmware'
+collection = 'product'
 
 
-website = "https://www.rockchipfirmware.com/firmware-downloads"
+
+def main():
+	
+	links = set()
+	
+	url = "https://www.rockchipfirmware.com/"
+	ending = "firmware-downloads"
+	current = 1
+
+	while current <= 7:
+	
+		url = "https://www.rockchipfirmware.com/" + str(ending)
+
+		# fetch raw html content
+		page = requests.get(url)
+
+		# parse the html content
+		soup = BeautifulSoup(page.content, 'html.parser')
+
+		soup.prettify()
+	
+
+		table = soup.find('table')
+		
+		table_rows = table.find_all('tr')
+
+		#for tr in table_rows:
+			#a = tr.find_all('a')
+			#row = [i.text.strip() for i in a]
+			#print(row)
+	
+		for link in soup.findAll('a')[6:-14]:
+			links.add(link.get('href'))
+		
+		if (current != 7):
+			ending = soup.find(title="Go to next page").get('href')
+			print(ending)
+		
+		
+		current += 1
+	
+	
+	links.remove('cdn-cgi\\l\\email-protection')
+	
+	for each in links:
+		
+		
+
+if __name__ == "__main__":
+	main()
+
+	
 
 
-result = requests.get(website)
 
-print(result.status_code)
 
-content = result.text
-
-soup = BeautifulSoup(content, 'lxml')
-
-print("kobe")
-print(content)
