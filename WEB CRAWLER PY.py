17fe7f3b1b8ee75efdf8611fc89e7790f1acc18b
“Eli <owen.astrachan@duke.edu>
2020-05-10 21:01:01 -0400

    import packages and wrote base code

--- WEB CRAWLER PY.py
+++ WEB CRAWLER PY.py
@@ -1,2 +1,27 @@
 import pymongo
+import requests
+from bs4 import BeautifulSoup
 
+#Establish connection with default host and port 
+client = MongoClient()
+
+#Create database
+db = client.firmware_db
+
+#Create a collection
+collection = db.firmware_collection
+
+
+website = "https://www.rockchipfirmware.com/firmware-downloads"
+
+
+result = requests.get(website)
+
+print(result.status_code)
+
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+
+print("kobe")
+print(content)
