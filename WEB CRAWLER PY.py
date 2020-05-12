2840dc9f77d1ad0366561f16b2bd96a6bd612e84
Eli Feder <esf10@duke.edu>
2020-05-11 22:22:54 -0400

    takes too long to run, might be coding for the wrong thing

--- WEB CRAWLER PY.py
+++ WEB CRAWLER PY.py
@@ -28,32 +28,53 @@ def main():
 		# parse the html content
 		soup = BeautifulSoup(page.content, 'html.parser')
 
-		soup.prettify()
-	
 
-		table = soup.find('table')
-		
-		table_rows = table.find_all('tr')
-
-		#for tr in table_rows:
-			#a = tr.find_all('a')
-			#row = [i.text.strip() for i in a]
-			#print(row)
 	
 		for link in soup.findAll('a')[6:-14]:
+		
 			links.add(link.get('href'))
 		
 		if (current != 7):
 			ending = soup.find(title="Go to next page").get('href')
-			print(ending)
+			
 		
 		
 		current += 1
+		
 	
 	
-	links.remove('cdn-cgi\\l\\email-protection')
 	
 	for each in links:
+		
+		if str(each).replace("\\", "/") == "cdn-cgi/l/email-protection":
+			continue
+		
+		
+		url = "https://www.rockchipfirmware.com/firmware-downloads" + "/" + str(each).replace("\\", "/")
+		
+		dict = {}
+		
+		# fetch raw html content
+		page = requests.get(url)
+
+		# parse the html content
+		soup = BeautifulSoup(page.content, 'html.parser')
+		
+		
+		for value in soup.findAll("div", {"class":"field-item even"}):
+			print("test")
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
 		
 		
 
