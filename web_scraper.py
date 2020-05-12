c671d3fd857bb82c8bb6b11713c7abfe9885f147
esfeder <esf10@duke.edu>
2020-05-11 23:42:59 -0400

    downloading files started

--- web_scraper.py
+++ web_scraper.py
@@ -4,32 +4,39 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+
 host = 'localhost'
 database = 'firmware'
 collection = 'product'
 
 
-
 def main():
 	
-	url = "https://www.rockchipfirmware.com/node/267"
+	url = 'https://www.rockchipfirmware.com/node/112'
 	
 	# fetch raw html content
 	page = requests.get(url)
 
 	# parse the html content
 	soup = BeautifulSoup(page.content, 'html.parser')
-
-
 	
-	for link in soup.findAll('a')[6:-14]:
-		
-		links.add(link.get('href'))
-		
-
+	# firmware files to be downloaded are zip files
+	filetype = '.zip'
+	domain = 'https://www.rockchipfirmware.com/'
 	
-	for value in soup.findAll("div", {"class":"field-item even"}):
-            print("test")
+	for link in soup.findAll('a'):
+		download = link.get('href')
+		if filetype in download:
+			with open('X5_box_4.1.1_1227.zip', 'wb') as file:
+				response = requests.get(domain + download)
+				file.write(response.content) 
+	
+	dict = {}
+	
+	
+
+	#for value in soup.findAll("div", {"class":"field-item even"}):
+            #print(value.get_text())
 
 
 
