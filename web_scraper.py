6461c5a66cb7e9191742afc8280fbd601b7cac79
esfeder <esf10@duke.edu>
2020-05-12 00:56:21 -0400

    finished metadata collection

--- web_scraper.py
+++ web_scraper.py
@@ -12,7 +12,7 @@ collection = 'product'
 
 def main():
 	
-	url = 'https://www.rockchipfirmware.com/node/267'
+	url = 'https://www.rockchipfirmware.com/content/virgo-ace-tablet'
 	
 	# fetch raw html content
 	page = requests.get(url)
@@ -22,9 +22,10 @@ def main():
 	
 	# firmware files to be downloaded are zip files
 	filetype = '.zip'
+
 	domain = 'https://www.rockchipfirmware.com/'
 	
-	for link in soup.findAll('a'):
+	'''for link in soup.findAll('a'):
 		download = link.get('href')
 		if filetype in download:
 
@@ -33,14 +34,27 @@ def main():
 			
 			with open(link_clean, 'wb') as file:
 				response = requests.get(domain + download)
-				file.write(response.content) 
+				file.write(response.content) '''
 	
 	dict = {}
 	
+	key_list = ['Device name', 'Submitted by']
+	val_list = []
 	
 
-	#for value in soup.findAll("div", {"class":"field-item even"}):
-            #print(value.get_text())
+	for value in soup.findAll("div", {"class":"field-label"}):
+		key_list.append(value.get_text()[:-2])
+
+	for value in soup.findAll("div", {"class":"field-item even"}):
+		val_list.append(value.get_text())
+
+
+	final = {k : v for k,v in zip(key_list, val_list)}
+	
+							
+
+		
+			
 
 
 
